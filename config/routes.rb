aebb5e6df0adc906fdae41bf1c8a613672d23d39
Ubuntu <ubuntu@ip-172-31-8-230.ap-northeast-1.compute.internal>
2022-08-05 14:51:05 +0000

    Create integration test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'goals#index'
+  root 'application#hello'
   
-  get '/new', to: 'goals#new'
-  get '/edit', to: 'goals#edit'
-  
-  get 'signup', to: 'users#new'
+  get '/signup', to: 'users#new'
   
   resources :users
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
+  
+  test "should get home" do
+    get root_path
     assert_response :success
   end
-
+  
+  test "should get new" do
+    get new_user_path
+    assert_response :success
+  end
+  
 end
