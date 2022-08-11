9d9c44f65d644cc79c51aaf0a145e72be901a442
Ubuntu <ubuntu@ip-172-31-8-230.ap-northeast-1.compute.internal>
2022-08-11 08:37:22 +0000

    Implement basic login

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   root 'application#hello'
   
   get '/signup', to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   
   resources :users
+  
+  
 end
