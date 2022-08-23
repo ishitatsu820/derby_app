d245cce179a80b6099d2781bba0de54d0532655c
Ubuntu <ubuntu@ip-172-31-8-230.ap-northeast-1.compute.internal>
2022-08-23 12:40:59 +0000

    Create user authorization

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -10,6 +10,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   
   
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: {user: { name: "",
@@ -20,9 +21,10 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_template 'users/edit'
   end
   
-  test "successful edit" do
-    get edit_user_path(@user) 
-    assert_template 'users/edit'
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
     name  = "Foo Bar"
     email = "foo@bar.com"
     goal = "machomacho"
