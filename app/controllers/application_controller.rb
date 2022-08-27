58c6d121a701fb4c11452e144e26fce1c712b5d7
ASUKA <ishitatsu820@gmail.com>
2022-08-27 01:32:13 +0000

    Update root

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     def hello
-      render html: "hello, world!"
+      render html: "hello, world! I'm TEARAKOYA DERBY APP"
     end
     
     include SessionsHelper
